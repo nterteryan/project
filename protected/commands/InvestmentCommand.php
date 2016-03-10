02c6638973eb3a7a50f7732bd928ff024b238705
Hovo <hovog83@gmail.com>
2016-03-10 18:24:54 +0400

    3/10 UerRariff

--- protected/commands/InvestmentCommand.php
+++ protected/commands/InvestmentCommand.php
@@ -2,19 +2,20 @@
 
 class InvestmentCommand extends CConsoleCommand {
     public function run() {
-        // here we are doing what we need to do
         $model = new UserTariff();
-        $userTariffList =  $model->getUserTariffList(array("status" => "ACTIVE"));
+        $userTariffList =  $model->getUserTariffListForCommand();
         foreach($userTariffList as $key => $value){
-            $originalDate = $value->created_date;
-            $str= "+".$value->close_month." month ";
-            $newDate = date("Y-m-d", strtotime($str,strtotime($originalDate)));
-            if($newDate <=  date("Y-m-d") ){
-                $model->model()->updateByPk($value->id, array('status' => "BLOCKED"));
+            $time = strtotime( $value->created_date);
+            $close_date = date("Y-m-d", strtotime("+ ".$value->close_month." month", $time));
+            $fina = strtotime($close_date);
+            if($fina < strtotime(date("Y-m-d")) ){
+                $value->status = "CLOSED";
+                $value->update();
+           }
+            if($fina >= strtotime(date("Y-m-d")) ){
+                $amount_percent = $value->amount_percent+($value->amount *($value->percent/100));
+                $model->model()->updateByPk($value->id, array('amount_percent' => $amount_percent ));
             }
-            $amount_percent = $value->amount_percent+($value->amount *($value->percent/100));
-            $model->model()->updateByPk($value->id, array('amount_percent' => $amount_percent ));
         }
-
     }
 }
